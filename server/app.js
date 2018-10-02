96bd094f8f48c5c1dce6c6ac03b12c27ca20a9e1
Florian Orpelière <florian.orpeliere@dailymotion.com>
2018-10-02 14:42:14 +0200

    Add new endpoint for characters, add nodemon, add apikey from file

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -7,13 +8,24 @@ const apiRouter = require('./routes/api');
 
 const app = express();
 
+const addBlizzardApiKey = () => {
+    if(fs.existsSync('./KEY')){
+        const key = fs.readFileSync('./KEY', {encoding: 'utf8'});
+        app.set('apikey', key.trim());
+    } else {
+        console.error(`The file KEY containing api key for blizzard api doesn't exist`);
+        process.exit(1);
+    }
+}
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+
+addBlizzardApiKey();
 
 app.use('/', indexRouter);
-app.use('/api', apiRouter);
+app.use('/api', apiRouter(app));
 
 module.exports = app;
--- server/routes/api.js
+++ server/routes/api.js
@@ -3,50 +3,94 @@ const axios = require('axios');
 const router = express.Router();
 
 const characters = ['Yxïon', 'Sämàel', 'Zesstra'];
-const apiKey = 'apikey=XXX';
 const local = 'locale=fr_FR';
+const realm = 'Dalaran';
 const host = 'https://eu.api.battle.net/wow';
 
-/* GET home page. */
-router.get('/characters/professions', (req, res) => {
-    const promises = characters.map(character =>
-        axios.get(`${host}/character/Dalaran/${encodeURIComponent(character)}?fields=professions&${local}&${apiKey}`)
-            .then(response => response.data));
-    Promise.all(promises)
-        .then(responses => {
-            responses.forEach(character => {
-               const primaryProfessions = character.professions.primary;
-               character.professions.primary = primaryProfessions.reduce((acc, profession) => {
-                   if(!acc[profession.icon]){
-                       acc[profession.icon] = []
-                   }
-                   acc[profession.icon].push(profession);
-                   return acc;
-               }, {});
-            });
-            res.json(responses);
-        })
-        .catch(err => {
-            console.warn(err);
-            res.status(500).json({error: 'Internal error'});
-        });
-});
+const referential = {};
 
-router.get('/referential', (req, res) => {
+const getReferential = (apiKey) => {
     const races = axios.get(`${host}/data/character/races?${local}&${apiKey}`).then(response => response.data);
     const classes = axios.get(`${host}/data/character/classes?l${local}&${apiKey}`).then(response => response.data);
     Promise.all([races, classes])
         .then(responses => {
-            const response = {}    opacity: 0.5;
-            /* cursor: none; */
-            pointer-events: none;
-            responses.forEach(referentialFromWow => Object.assign(response, referentialFromWow));
-            res.json(response);
+            responses.forEach(referentialFromWow => Object.assign(referential, referentialFromWow));
         })
         .catch(err => {
-            console.warn(err);
-            res.status(500).json({error: 'Internal error'});
+            console.error(err);
+            console.error(`We can't load the referential. See errors above`);
+            process.exit(1);
         });
-});
+}
 
-module.exports = router;
+const enrishResponse = (response) => {
+    if(Array.isArray(response)){
+        const enrishResponses = response.map(character => {
+            const enrishCharacter = Object.assign({}, character);
+            if(Number.isInteger(character.class)) { // Is defined
+                enrishCharacter.className = referential.classes.filter(({ id }) => id === character.class)[0].name;
+            }
+            if(Number.isInteger(character.race)) { // Is defined
+                enrishCharacter.raceName = referential.races.filter(({ id }) => id === character.race)[0].name;
+            }
+            return enrishCharacter;
+        });
+
+        return enrishResponses;
+    }
+
+    return response;
+}
+
+/* GET home page. */
+module.exports = (app) => {
+    const apiKey = `apikey=${app.get('apikey')}`;
+
+    getReferential(apiKey);
+
+    router.get('/characters', (req, res) => {
+        const promises = characters.map(character =>
+        axios.get(`${host}/character/${realm}/${encodeURIComponent(character)}?${local}&${apiKey}`)
+                .then(response => response.data));
+        Promise.all(promises)
+            .then(responses => {
+                const enrishedResponse = enrishResponse(responses);
+
+                res.json(enrishedResponse);
+            })
+            .catch(err => {
+                console.warn(err);
+                res.status(500).json({error: 'Internal error'});
+            });
+    });
+
+    router.get('/characters/professions', (req, res) => {
+        const promises = characters.map(character =>
+            axios.get(`${host}/character/${realm}/${encodeURIComponent(character)}?fields=professions&${local}&${apiKey}`)
+                .then(response => response.data));
+        Promise.all(promises)
+            .then(responses => {
+                responses.forEach(character => {
+                    const primaryProfessions = character.professions.primary;
+                    character.professions.primary = primaryProfessions.reduce((acc, profession) => {
+                        if(!acc[profession.icon]){
+                            acc[profession.icon] = []
+                        }
+                        acc[profession.icon].push(profession);
+                        return acc;
+                    }, {});
+                });
+                res.json(responses);
+            })
+            .catch(err => {
+                console.warn(err);
+                res.status(500).json({error: 'Internal error'});
+            });
+    });
+
+    router.get('/referential', (req, res) => {
+        res.json(referential);
+    });
+
+    return router;
+};
